76b87a07fd015399f4b84ed8f10b088b02204062
divineLush <alvipath@yahoo.com>
2025-05-05 00:52:47 +0700

    bfs & dfs

---
+++ tree/bfs.js
@@ -0,0 +1,34 @@
+const graph = {
+  'A': ['B', 'C'],
+  'B': ['D', 'E'],
+  'C': ['F'],
+  'D': [],
+  'E': ['F'],
+  'F': []
+}
+
+const bfs = (graph, startNode) => {
+  const visited = new Set()
+  const queue = [startNode]
+
+  while (queue.length) {
+    const currentNode = queue.shift()
+
+    if (visited.has(currentNode)) {
+      continue
+    }
+
+    console.log(currentNode)
+    visited.add(currentNode)
+
+    for (const neighbor of graph[currentNode]) {
+      if (visited.has(neighbor)) {
+        continue
+      }
+
+      queue.push(neighbor)
+    }
+  }
+}
+
+bfs(graph, 'A')
---
+++ tree/dfs.js
@@ -0,0 +1,51 @@
+const graph = {
+  'A': ['B', 'C'],
+  'B': ['D', 'E'],
+  'C': ['F'],
+  'D': [],
+  'E': ['F'],
+  'F': []
+}
+
+const dfs = (node, visited = new Set()) => {
+  if (!node || visited.has(node)) {
+    return
+  }
+
+  console.log(node)
+  visited.add(node)
+
+  for (const neighbor of graph[node]) {
+    dfs(neighbor, visited)
+  }
+}
+
+dfs('A')
+
+const iterativeDfs = (graph, startNode) => {
+  const visited = new Set()
+  const stack = [startNode]
+
+  while (stack.length) {
+    const currentNode = stack.pop()
+
+    if (visited.has(currentNode)) {
+      continue
+    }
+
+    console.log(currentNode)
+    visited.add(currentNode)
+
+    for (const neighbor of graph[currentNode].toReversed()) {
+      if (visited.has(neighbor)) {
+        continue
+      }
+
+      stack.push(neighbor)
+    }
+  }
+}
+
+console.log('================')
+
+iterativeDfs(graph, 'A')
